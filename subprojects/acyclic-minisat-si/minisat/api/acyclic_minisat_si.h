a3dde3ead65b97bd621dcbe4529f197bb28b4fb5
XiaoZhi <zhiheng-cai@smail.nju.edu.cn>
2024-04-27 15:55:33 +0800

    integrate si checking

---
+++ subprojects/acyclic-minisat-si/minisat/api/acyclic_minisat_si.h
@@ -0,0 +1,16 @@
+#ifndef MINISAT_ACYCLIC_MINISAT_H
+#define MINISAT_ACYCLIC_MINISAT_H
+
+#include <utility>
+#include <vector>
+
+#include "minisat/api/acyclic_minisat_types.h"
+
+namespace MinisatSI {
+
+// precondition: known_graph and constraints must be remapped
+bool am_solve(int n_vertices, const KnownGraph &known_graph, const Constraints &constraints);
+
+} // namespace MinisatSI
+
+#endif // MINISAT_ACYCLIC_MINISAT_H
