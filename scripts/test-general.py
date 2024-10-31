5193b22973e551193d6eb19bc4088e3e70fbb43c
rikka <rikka@tsinghua.lab>
2024-10-31 15:46:29 +0800

    + histories

--- scripts/test-general.py
+++ scripts/test-general.py
@@ -48,8 +48,8 @@ logging.info(f'root path = {root_path}')
 # history_path = os.path.join(root_path, 'history', '{}-logs'.format(history_type), 'uv')
 # history_path = os.path.join(root_path, 'history', 'ser', '{}-logs'.format(history_type), 'no-uv', 'polysi-fig7-like')
 # history_path = os.path.join(root_path, 'history', 'ser', 'general')
-# history_path = os.path.join(root_path, 'history', 'ser', 'general-pldi')
-history_path = os.path.join(root_path, 'history', 'ser', 'tmp')
+history_path = os.path.join(root_path, 'history', 'ser', 'general-pldi')
+# history_path = os.path.join(root_path, 'history', 'ser', 'tmp')
 # history_path = os.path.join(root_path, 'history', '{}-logs'.format(history_type), 'no-uv', 'scalability4')
 logging.info(f'history path = {history_path}')
 
@@ -64,7 +64,7 @@ solver = 'acyclic-minisat'
 assert solver == 'acyclic-minisat' or solver == 'monosat' or solver == 'z3' or solver == 'monosat-baseline'
 logging.info(f'solver = {solver}')
 
-pruning_method = 'basic'
+pruning_method = 'none'
 assert pruning_method == 'fast' or pruning_method == 'normal' or pruning_method == 'none' or pruning_method == 'unit' or pruning_method == 'basic'
 logging.info(f'pruning method = {pruning_method}')
 
